f0079456562063e6350a68712b01258d70a0c3bb
xtexChooser <xtexchooser@duck.com>
2023-04-02 17:36:16 +0800

    vendor: fix py syntax

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -23,19 +23,19 @@ import os
 import re
 import sys
 try:
-  # For python3
-  import urllib.error
-  import urllib.parse
-  import urllib.request
+    # For python3
+    import urllib.error
+    import urllib.parse
+    import urllib.request
 except ImportError:
-  # For python2
-  import imp
-  import urllib2
-  import urlparse
-  urllib = imp.new_module('urllib')
-  urllib.error = urllib2
-  urllib.parse = urlparse
-  urllib.request = urllib2
+    # For python2
+    import imp
+    import urllib2
+    import urlparse
+    urllib = imp.new_module('urllib')
+    urllib.error = urllib2
+    urllib.parse = urlparse
+    urllib.request = urllib2
 
 from xml.etree import ElementTree
 
@@ -61,19 +61,23 @@ try:
 
     if authtuple:
         auth_string = ('%s:%s' % (authtuple[0], authtuple[2])).encode()
-        githubauth = base64.encodestring(auth_string).decode().replace('\n', '')
+        githubauth = base64.encodestring(
+            auth_string).decode().replace('\n', '')
     else:
         githubauth = None
 except:
     githubauth = None
 
+
 def add_auth(githubreq):
     if githubauth:
-        githubreq.add_header("Authorization","Basic %s" % githubauth)
+        githubreq.add_header("Authorization", "Basic %s" % githubauth)
+
 
 if not depsonly:
-    // @TODO: upstream uses LineageOS/mirror
-    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:chitangUI+in:name+fork:true" % device)
+    # @TODO: upstream uses LineageOS/mirror
+    githubreq = urllib.request.Request(
+        "https://api.github.com/search/repositories?q=%s+user:chitangUI+in:name+fork:true" % device)
     add_auth(githubreq)
     try:
         result = json.loads(urllib.request.urlopen(githubreq).read().decode())
@@ -87,7 +91,9 @@ if not depsonly:
         repositories.append(res)
 
 local_manifests = r'.repo/local_manifests'
-if not os.path.exists(local_manifests): os.makedirs(local_manifests)
+if not os.path.exists(local_manifests):
+    os.makedirs(local_manifests)
+
 
 def exists_in_tree(lm, path):
     for child in lm.getchildren():
@@ -96,6 +102,8 @@ def exists_in_tree(lm, path):
     return False
 
 # in-place prettyprint formatter
+
+
 def indent(elem, level=0):
     i = "\n" + level*"  "
     if len(elem):
@@ -111,6 +119,7 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
+
 def get_manifest_path():
     '''Find the current manifest path
     In old versions of repo this is at .repo/manifest.xml
@@ -124,11 +133,13 @@ def get_manifest_path():
     except IndexError:
         return ".repo/manifests/{}".format(m.find("include").get("name"))
 
+
 def get_default_revision():
     m = ElementTree.parse(get_manifest_path())
     d = m.findall('default')[0]
     r = d.get('revision')
     return r.replace('refs/heads/', '').replace('refs/tags/', '')
+
 
 def get_from_manifest(devicename):
     try:
@@ -142,6 +153,7 @@ def get_from_manifest(devicename):
             return localpath.get("path")
 
     return None
+
 
 def is_in_manifest(projectpath):
     try:
@@ -178,6 +190,7 @@ def is_in_manifest(projectpath):
 
     return False
 
+
 def add_to_manifest(repositories):
     try:
         lm = ElementTree.parse(".repo/local_manifests/roomservice.xml")
@@ -195,11 +208,11 @@ def add_to_manifest(repositories):
             continue
 
         print('Adding dependency: %s -> %s' % (repo_name, repo_target))
-        project = ElementTree.Element("project", attrib = {
+        project = ElementTree.Element("project", attrib={
             "path": repo_target,
             "remote": "github",
             "name": "%s" % repo_name,
-            "revision": repo_revision })
+            "revision": repo_revision})
         lm.append(project)
 
     indent(lm, 0)
@@ -209,6 +222,7 @@ def add_to_manifest(repositories):
     f = open('.repo/local_manifests/roomservice.xml', 'w')
     f.write(raw_xml)
     f.close()
+
 
 def fetch_dependencies(repo_path):
     print('Looking for dependencies in %s' % repo_path)
@@ -227,13 +241,19 @@ def fetch_dependencies(repo_path):
                 syncable_repos.append(dependency['target_path'])
                 if 'branch' not in dependency:
                     try:
-                        dependency['branch'] = get_default_or_fallback_revision('chitangUI/' + dependency['repository'])
-                        dependency['repository'] = 'chitangUI/' + dependency['repository']
-                        print("%s is resolved from chitangUI" % (dependency['repository']))
+                        dependency['branch'] = get_default_or_fallback_revision(
+                            'chitangUI/' + dependency['repository'])
+                        dependency['repository'] = 'chitangUI/' + \
+                            dependency['repository']
+                        print("%s is resolved from chitangUI" %
+                              (dependency['repository']))
                     except:
-                        dependency['branch'] = get_default_or_fallback_revision('LineageOS/' + dependency['repository'])
-                        dependency['repository'] = 'LineageOS/' + dependency['repository']
-                        print("%s is resolved from LineageOS" % (dependency['repository']))
+                        dependency['branch'] = get_default_or_fallback_revision(
+                            'LineageOS/' + dependency['repository'])
+                        dependency['repository'] = 'LineageOS/' + \
+                            dependency['repository']
+                        print("%s is resolved from LineageOS" %
+                              (dependency['repository']))
             verify_repos.append(dependency['target_path'])
 
             if not os.path.isdir(dependency['target_path']):
@@ -254,38 +274,45 @@ def fetch_dependencies(repo_path):
     for deprepo in verify_repos:
         fetch_dependencies(deprepo)
 
+
 def has_branch(branches, revision):
     return revision in [branch['name'] for branch in branches]
 
+
 def get_default_revision_no_minor():
     return get_default_revision().rsplit('.', 1)[0]
+
 
 def get_default_or_fallback_revision(repo_name):
     default_revision = "tehc"
     print("Default revision: %s" % default_revision)
     print("Checking branch info")
 
-    githubreq = urllib.request.Request("https://api.github.com/repos/" + repo_name + "/branches")
+    githubreq = urllib.request.Request(
+        "https://api.github.com/repos/" + repo_name + "/branches")
     add_auth(githubreq)
     result = json.loads(urllib.request.urlopen(githubreq).read().decode())
     if has_branch(result, default_revision):
         return default_revision
 
-    fallbacks = [ get_default_revision(), get_default_revision_no_minor() ]
+    fallbacks = [get_default_revision(), get_default_revision_no_minor()]
     if os.getenv('ROOMSERVICE_BRANCHES'):
-        fallbacks += list(filter(bool, os.getenv('ROOMSERVICE_BRANCHES').split(' ')))
+        fallbacks += list(filter(bool,
+                          os.getenv('ROOMSERVICE_BRANCHES').split(' ')))
 
     for fallback in fallbacks:
         if has_branch(result, fallback):
             print("Using fallback branch: %s" % fallback)
             return fallback
 
-    print("Default revision %s not found in %s. Bailing." % (default_revision, repo_name))
+    print("Default revision %s not found in %s. Bailing." %
+          (default_revision, repo_name))
     print("Branches found:")
     for branch in [branch['name'] for branch in result]:
         print(branch)
     print("Use the ROOMSERVICE_BRANCHES environment variable to specify a list of fallback branches.")
     sys.exit()
+
 
 if depsonly:
     repo_path = get_from_manifest(device)
@@ -301,12 +328,15 @@ else:
         repo_name = repository['name']
         if re.match(r"^android_device_[^_]*_" + device + "$", repo_name):
             print("Found repository: %s" % repository['name'])
-            
-            manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
-            repo_path = "device/%s/%s" % (manufacturer, device)
-            revision = get_default_or_fallback_revision('chitangUI/' + repo_name)
 
-            device_repository = {'repository':'chitangUI/' + repo_name,'target_path':repo_path,'branch':revision}
+            manufacturer = repo_name.replace(
+                "android_device_", "").replace("_" + device, "")
+            repo_path = "device/%s/%s" % (manufacturer, device)
+            revision = get_default_or_fallback_revision(
+                'chitangUI/' + repo_name)
+
+            device_repository = {'repository': 'chitangUI/' +
+                                 repo_name, 'target_path': repo_path, 'branch': revision}
             add_to_manifest([device_repository])
 
             print("Syncing repository to retrieve project.")
